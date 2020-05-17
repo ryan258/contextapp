c207c76d844baf06634eca50288462533bf84123
Ryan Johnson <ryan@258webco.com>
2020-05-16 23:20:05 -0500

    Consume Context with Class components.

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 class BookList extends Component {
+  static contextType = ThemeContext;
+
   render() {
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
     return (
-      <div className="book-list">
+      <div
+        className="book-list"
+        style={{ color: theme.syntax, background: theme.bg }}
+      >
         <ul>
-          <li>The Shining</li>
-          <li>Needful Things</li>
-          <li>Green Eggs and Ham</li>
+          <li style={{ background: theme.ui }}>The Shining</li>
+          <li style={{ background: theme.ui }}>Needful Things</li>
+          <li style={{ background: theme.ui }}>Green Eggs and Ham</li>
         </ul>
       </div>
     );
